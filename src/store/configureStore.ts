d5247428611fbbfb805eaff66547acce2b84b248
Seonghui <stellashyou@gmail.com>
2019-11-13 12:33:18 +0900

    state와 로컬스토리지 연동

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,11 +1,20 @@
 import modules, { StoreState } from "./modules";
 import { createStore, Store } from "redux";
+import { loadState, saveState } from '../utils/localStorage'
 
 export default function configureStore(): Store<StoreState> {
+  const persistedState = loadState();
   const store = createStore(
     modules,
+    persistedState,
     (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  store.subscribe(() => {
+    saveState({
+      todos: store.getState().todos
+    });
+  });
   return store;
 }
