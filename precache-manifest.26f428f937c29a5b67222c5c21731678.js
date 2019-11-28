b0fb72b104896c8406057af2be906ae8c8a27ff0
Stella Yu <stellashyou@gmail.com>
2019-11-28 17:54:22 +0900

    Updates

---
+++ precache-manifest.26f428f937c29a5b67222c5c21731678.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d79c31616485f969d9665d0bd4d976",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "3ded41dccd13c59eae8f",
+    "url": "/todo-list/static/css/main.6183dc3b.chunk.css"
+  },
+  {
+    "revision": "b63f5873f9bf2c3307ef",
+    "url": "/todo-list/static/js/2.b5acdd10.chunk.js"
+  },
+  {
+    "revision": "3ded41dccd13c59eae8f",
+    "url": "/todo-list/static/js/main.dc611f28.chunk.js"
+  },
+  {
+    "revision": "69640fa782c68a1174f8",
+    "url": "/todo-list/static/js/runtime-main.b5de13ca.js"
+  }
+]);
